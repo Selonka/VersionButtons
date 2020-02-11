66f26cba321de68b062fcb099e4a9cc7f23e5739
Selonka <martinkloeppel@yahoo.de>
2020-02-11 22:43:41 +0100

    Get everything inline
    Get the buttons inline, not quite yet perfect

--- VersionButtons/files/VersionButton.js
+++ VersionButtons/files/VersionButton.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
+    // Get all the stuff you need
     var versionDiv = document.getElementById('project-versions-div');
-    var copyForm = versionDiv.getElementsByClassName('widget-toolbox padding-8 clearfix')[0];
-    var formToClone = document.getElementById('manage-project-add-version-form').cloneNode(true);
-    copyForm.appendChild(formToClone);
+    var copyToolBoxDiv = versionDiv.getElementsByClassName('widget-toolbox padding-8 clearfix')[0];
+    var copyForm = document.getElementById('manage-project-version-copy-form');
+    var addForm = document.getElementById('manage-project-add-version-form').cloneNode(true);
+    var wrapperCopyForm = document.createElement('div');
+    var wrapperAddForm = document.createElement('div');
+   
+    //set new class definition
+    copyToolBoxDiv.classList.add("form-row");
+    wrapperCopyForm.classList.add("form-group", "col-md-6");
+    wrapperAddForm.classList.add("form-group", "col-md-6");
+    
+    //Build new structure
+    copyToolBoxDiv.replaceChild(wrapperCopyForm,copyForm);
+    wrapperCopyForm.appendChild(copyForm);
+    copyToolBoxDiv.appendChild(wrapperAddForm);
+    wrapperAddForm.appendChild(addForm);
 });
 
